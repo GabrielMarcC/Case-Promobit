4dbbe33bfdedaaefb9d130bb887c6785e3656a19
Gabriel <gabrielmarcelino099@gmail.com>
2023-06-12 18:29:07 -0300

    changed config files

--- src/services/services.ts
+++ src/services/services.ts
@@ -8,10 +8,16 @@ import {
   RecommendationsTypes,
 } from "../types";
 
+const api = import.meta.env.VITE_BASE_URL;
+
+console.log(api);
+
 export const MoviesServices = () => {
   const handlePopularMovies = async (page?: number): Promise<PopularMovies> => {
     const response = await axios.get<PopularMovies>(
-      `${config.base_URL}/popular?api_key=${config.apiKey}&language=pt-BR&page=${page}`
+      `${import.meta.env.VITE_BASE_URL}/popular?api_key=${
+        import.meta.env.VITE_API_KEY
+      }&language=pt-BR&page=${page}`
     );
     return response.data;
   };
@@ -20,7 +26,9 @@ export const MoviesServices = () => {
     id?: string
   ): Promise<MoviesDetailsTypes> => {
     const response = await axios.get<MoviesDetailsTypes>(
-      `${config.base_URL}/${id}?api_key=${config.apiKey}&language=pt-BR&include_image_language=pt-BR&append_to_response=videos,images,release_dates`
+      `${import.meta.env.VITE_BASE_URL}/${id}?api_key=${
+        import.meta.env.VITE_API_KEY
+      }&language=pt-BR&include_image_language=pt-BR&append_to_response=videos,images,release_dates`
     );
 
     return response.data;
@@ -28,7 +36,9 @@ export const MoviesServices = () => {
 
   const handleCredits = async (id?: string): Promise<CreditsTypes> => {
     const response = await axios.get<CreditsTypes>(
-      `${config.base_URL}/${id}/credits?api_key=${config.apiKey}&language=pt-BR`
+      `${import.meta.env.VITE_BASE_URL}/${id}/credits?api_key=${
+        import.meta.env.VITE_API_KEY
+      }&language=pt-BR`
     );
     return response.data;
   };
@@ -37,7 +47,9 @@ export const MoviesServices = () => {
     id?: string
   ): Promise<RecommendationsTypes> => {
     const response = await axios.get<RecommendationsTypes>(
-      `${config.base_URL}/${id}/recommendations?api_key=${config.apiKey}&language=pt-BR`
+      `${import.meta.env.VITE_BASE_URL}/${id}/recommendations?api_key=${
+        import.meta.env.VITE_API_KEY
+      }&language=pt-BR`
     );
 
     return response.data;
@@ -45,7 +57,9 @@ export const MoviesServices = () => {
 
   const handleGenres = async (): Promise<GenresResponse> => {
     const response = await axios.get<GenresResponse>(
-      `https://api.themoviedb.org/3/genre/movie/list?api_key=${config.apiKey}&language=pt-Br`
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${
+        import.meta.env.VITE_API_KEY
+      }&language=pt-Br`
     );
     return response.data;
   };
